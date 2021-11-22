cb65b5f6c23d52d5bf1391b8259ba3def340f1e8
Dirk Eddelbuettel <edd@debian.org>
2021-11-22 17:17:55 -0600

    more small adjustments for rcppquanticcia -> qlcal change

--- src/dates.cpp
+++ src/dates.cpp
@@ -1,22 +1,22 @@
 
-//  RcppQuantuccia -- R interface to QuantLib via Quantuccia
+//  QlCal -- R interface to QuantLib Calendars
 //
 //  Copyright (C) 2002 - 2021  Dirk Eddelbuettel <edd@debian.org>
 //
-//  This file is part of RcppQuantuccia
+//  This file is part of QlCal
 //
-//  RcppQuantuccia is free software: you can redistribute it and/or modify
+//  QlCal is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 2 of the License, or
 //  (at your option) any later version.
 //
-//  RcppQuantuccia is distributed in the hope that it will be useful,
+//  QlCal is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with RcppQuantuccia.  If not, see <http://www.gnu.org/licenses/>.
+//  along with QlCal.  If not, see <http://www.gnu.org/licenses/>.
 
 
 // Taken from RQuantLib and adapted
