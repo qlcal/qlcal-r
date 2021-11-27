0cfb1fc8f5be76bc615fedd0a96b5df19950464e
Dirk Eddelbuettel <edd@debian.org>
2021-11-27 16:34:08 -0600

    update qldefines from QuantLib (leaving small delta)

--- src/ql/qldefines.hpp
+++ src/ql/qldefines.hpp
@@ -37,7 +37,7 @@
 
 #include <boost/config.hpp>
 #include <boost/version.hpp>
-#if BOOST_VERSION < 103900
+#if BOOST_VERSION < 104800
     #error using an old version of Boost, please update.
 #endif
 #if !defined(BOOST_ENABLE_ASSERT_HANDLER)
@@ -185,20 +185,45 @@
 
 
 // emit warning when using deprecated features
+// clang-format off
 #if defined(BOOST_MSVC)       // Microsoft Visual C++
-#define QL_DEPRECATED __declspec(deprecated)
-#elif defined(__GNUC__) || defined(__clang__)
-#define QL_DEPRECATED __attribute__((deprecated))
+#    define QL_DEPRECATED __declspec(deprecated)
+#    define QL_DEPRECATED_DISABLE_WARNING \
+        __pragma(warning(push))           \
+        __pragma(warning(disable : 4996))
+#    define QL_DEPRECATED_ENABLE_WARNING \
+        __pragma(warning(pop))
+#elif defined(__GNUC__)
+#    define QL_DEPRECATED __attribute__((deprecated))
+#    define QL_DEPRECATED_DISABLE_WARNING                               \
+        _Pragma("GCC diagnostic push")                                  \
+        _Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"")
+#    define QL_DEPRECATED_ENABLE_WARNING \
+        _Pragma("GCC diagnostic pop")
+#elif defined(__clang__)
+#    define QL_DEPRECATED __attribute__((deprecated))
+#    define QL_DEPRECATED_DISABLE_WARNING                                 \
+        _Pragma("clang diagnostic push")                                  \
+        _Pragma("clang diagnostic ignored \"-Wdeprecated-declarations\"")
+#    define QL_DEPRECATED_ENABLE_WARNING \
+        _Pragma("clang diagnostic pop")
 #else
-// we don't know how to enable it, just define the macro away
-#define QL_DEPRECATED
+// we don't know how to enable it, just define the macros away
+#    define QL_DEPRECATED
+#    define QL_DEPRECATED_DISABLE_WARNING
+#    define QL_DEPRECATED_ENABLE_WARNING
 #endif
+// clang-format on
+
 
 // until we stop supporting Visual C++ 2013
 #if defined(QL_PATCH_MSVC_2013)
 #  define QL_NOEXCEPT
+#  define QL_CONSTEXPR
 #else
 #  define QL_NOEXCEPT noexcept
+#  define QL_CONSTEXPR constexpr
 #endif
+
 
 #endif
