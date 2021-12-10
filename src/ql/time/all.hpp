b49e5925277ededbdb05160705b309060b5164ad
Dirk Eddelbuettel <edd@debian.org>
2021-12-09 19:12:58 -0600

    adjust ql/time/all.hpp too

--- src/ql/time/all.hpp
+++ src/ql/time/all.hpp
@@ -2,13 +2,13 @@
 #include <ql/time/businessdayconvention.hpp>
 #include <ql/time/calendar.hpp>
 #include <ql/time/date.hpp>
-#include <ql/time/dategenerationrule.hpp>
+// -- not needed  #include <ql/time/dategenerationrule.hpp>
 // -- not needed  #include <ql/time/daycounter.hpp>
 #include <ql/time/ecb.hpp>
 #include <ql/time/frequency.hpp>
 #include <ql/time/imm.hpp>
 #include <ql/time/period.hpp>
-#include <ql/time/schedule.hpp>
+// -- not needed  #include #include <ql/time/schedule.hpp>
 #include <ql/time/timeunit.hpp>
 #include <ql/time/weekday.hpp>
 
